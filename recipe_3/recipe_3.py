85a464862d14f3658a3af29093dca3865148f442
Tiago <tiago16tc@yahoo.com.br>
2017-05-17 19:56:20 +1200

    Recipe 3

---
+++ recipe_3/recipe_3.py
@@ -0,0 +1,25 @@
+#############################################################
+# What makes a good feature? - Machine Learning Recipes #3  #
+# https://www.youtube.com/watch?v=tNa99PG8hR8               #
+#############################################################
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+greyhounds = 500
+labs = 500
+
+np.random.seed(8)
+
+# randn returns a float between (-inf, +inf) following a normal distribution with mean 0 and standard deviation 1
+grey_height = 28 + 4 * np.random.randn(greyhounds)
+lab_height = 24 + 4 * np.random.randn(labs)
+
+plt.hist([grey_height, lab_height],
+         stacked=True,
+         color=["orange", "blue"],
+         label=["Greyhounds", "Labs"])
+plt.legend(prop={"size": 10})
+plt.xlabel("dog height")
+plt.ylabel("# of dogs")
+plt.show()
